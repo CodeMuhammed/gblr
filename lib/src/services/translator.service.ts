7cd4b02707d8d21b286c95e3074436edca4b0d8b
codemuhammed <codemuhammed@gmail.com>
2018-06-20 02:57:24 +0100

    feat(app): add default language when module is first initialized

--- lib/src/services/translator.service.ts
+++ lib/src/services/translator.service.ts
@@ -4,7 +4,24 @@ import { Language } from '../models/translator';
 
 @Injectable()
 export class TranslatorService {
-    constructor(@Inject('db') private afsDB) {}
+    constructor(@Inject('db') private afsDB) {
+       this.initLnguages();
+    }
+
+    async initLnguages() {
+        const languages = await this.getLanguages().take(1).toPromise();
+
+        if(languages.length == 0) {
+            const defaultLanguage: Language = new Language();
+
+            defaultLanguage.id = 'en';
+            defaultLanguage.name = 'english',
+            defaultLanguage.symbol = 'en',
+            defaultLanguage.segments = [];
+
+            this.updateLanguage(JSON.parse(JSON.stringify(defaultLanguage)));
+        }
+    }
 
     getLanguages(): Observable<Language[]> {
         return this.afsDB.colWithIds$('/languages')
