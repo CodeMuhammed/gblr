37d448aa5c3381a03bcc49dbbbe8b267afa77c66
codemuhammed <codemuhammed@gmail.com>
2018-07-03 11:36:08 +0100

    feat(app): fix css bug

--- lib/src/translate.module.ts
+++ lib/src/translate.module.ts
@@ -3,14 +3,13 @@ import { TranslatorComponent } from './components';
 import { NewLanguagePromptComponent, PromptDialog } from './entry-components';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { Mat_LOADERS } from './mat-loaders';
 import { TranslatorService, UserPromptsService } from './services';
 
 @NgModule({
     declarations: [TranslatorComponent, NewLanguagePromptComponent, PromptDialog],
     exports: [TranslatorComponent],
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, BrowserAnimationsModule, Mat_LOADERS.IMPORTS],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, Mat_LOADERS.IMPORTS],
     entryComponents: [
         NewLanguagePromptComponent,
         PromptDialog
